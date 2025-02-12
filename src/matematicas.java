73dcd26a37097c4b7db0e9e3d021e55d16617dba
Javiermartinez13 <franciscojavier.martinezfernan@usp.ceu.es>
2025-02-12 12:35:34 +0100

    Practica-1

---
+++ src/matematicas.java
@@ -0,0 +1,35 @@
+
+import java.util.Random;
+
+public class matematicas {
+
+    public static double generarNumeroPi(long pasos) {
+        long puntosEnCirculo = 0;
+
+        Random random = new Random();
+
+        for (long i = 0; i < pasos; i++) {
+            double x = random.nextDouble();  
+            double y = random.nextDouble();  
+
+            
+            double distanciaAlCentro = Math.sqrt(Math.pow(x - 0.5, 2) + Math.pow(y - 0.5, 2));
+
+            
+            if (distanciaAlCentro <= 0.5) {
+                puntosEnCirculo++;
+            }
+        }
+
+        
+        double aproximacionPi = 4.0 * puntosEnCirculo / pasos;
+
+        return aproximacionPi;
+    }
+
+    public static void main(String[] args) {
+        long pasos = 10000000;  // Puedes ajustar este valor según sea necesario
+        double aproximacionPi = generarNumeroPi(pasos);
+        System.out.println("Aproximación de pi después de " + pasos + " pasos: " + aproximacionPi);
+    }
+}
